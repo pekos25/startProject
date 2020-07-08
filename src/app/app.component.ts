c735db908971acdb16393fe44b917f855135d9b3
pekos25 <pekos25@gmail.com>
2020-07-08 19:23:19 +0200

    bootstrap jquery navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery' ;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   
+  ngOnInit(){
+    $('#year').text(new Date().getFullYear());
+  }
 }
